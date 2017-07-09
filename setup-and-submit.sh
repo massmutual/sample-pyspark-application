17debff1257fdbe31663d324c42ccf3074c9fc29
Nicholas Chammas <nicholas.chammas@gmail.com>
2017-07-09 15:39:57 -0400

    debug

--- setup-and-submit.sh
+++ setup-and-submit.sh
@@ -28,6 +28,9 @@ popd
 # which includes all our dependencies.
 export PYSPARK_PYTHON="venv/bin/python"
 
+which spark-submit
+echo $PATH
+
 spark-submit \
     --name "Sample Spark Application" \
     --master yarn \
